938f661689c1dcb677689d46d0e42800ec4d5c8c
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-08-20 12:05:08 -0400

    Added control flow

--- mayor_de_3.rb
+++ mayor_de_3.rb
@@ -1,6 +1,15 @@
 #Input variables
+num1 = ARGV[0].to_i
+num2 = ARGV[1].to_i
+num3 = ARGV[2].to_i
 
-num1 = ARGV[0].to_f
-num2 = ARGV[1].to_f
-num3 = ARGV[2].to_f
+#Control flow
+if num1 >= num2 && num1 >= num3
+    puts num1
+elsif num2 >= num3
+    puts num2
+else
+    puts num3
+end
+
 
